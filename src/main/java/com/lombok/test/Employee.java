b8034df08bc26bf462e480054d9877757905ef60
Kiran <ksekhara3@gatech.edu>
2022-04-11 08:47:08 +0530

    lombok

---
+++ src/main/java/com/lombok/test/Employee.java
@@ -0,0 +1,14 @@
+package com.lombok.test;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Employee {
+
+	private Integer empId;
+	private String empName;
+	private Double empSal;
+	
+}
